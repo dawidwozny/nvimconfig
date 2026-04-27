c351ea9cfa1c8ca00a3d23e564c6dbe9fbb43d91
Dawid Woźny <dawid.wozny@banqsoft.com>
2026-04-27 07:18:58 +0200

    seeing hidden

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,36 @@
+---@type LazySpec
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+        },
+      },
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+          grep = {
+            hidden = true,
+            ignored = true,
+          },
+          grep_word = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
+    },
+  },
+}
