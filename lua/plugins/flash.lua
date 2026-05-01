2825279535c6481e80a32f5b2f1ae95ba19d15ac
Dawid Woźny <dawid.wozny@banqsoft.com>
2026-05-01 15:49:21 +0200

    better colours

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -7,6 +7,40 @@ return {
           enabled = true,
         },
       },
+      -- Make jump labels more visible
+      label = {
+        -- Use uppercase letters for better visibility
+        uppercase = true,
+        -- Style for the jump labels
+        style = "overlay", -- or "inline", "eol"
+      },
     },
+    config = function(_, opts)
+      require("flash").setup(opts)
+
+      -- Get Catppuccin colors
+      local colors = require("catppuccin.palettes").get_palette()
+
+      -- Custom highlight groups using Catppuccin colors
+      vim.api.nvim_set_hl(0, "FlashLabel", {
+        fg = colors.base,      -- Dark text
+        bg = colors.peach,     -- Peach background (orange-ish)
+        bold = true,
+        italic = false,
+      })
+
+      vim.api.nvim_set_hl(0, "FlashMatch", {
+        fg = colors.mauve,     -- Purple text
+        bg = "NONE",
+        bold = true,
+        underline = true,
+      })
+
+      vim.api.nvim_set_hl(0, "FlashCurrent", {
+        fg = colors.base,      -- Dark text
+        bg = colors.green,     -- Green background
+        bold = true,
+      })
+    end,
   },
 }
